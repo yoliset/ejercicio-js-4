c4c7ffece22a533de8db7216b1d0f656bc95dd8f
Tatiana Limache Quispe <Tati34che@gmail.com>
2016-12-19 17:15:22 -0500

    Ejercicio terminado

---
+++ README.md
@@ -0,0 +1,14 @@
+# Ejercicio3
+Inicio : LLamada Telefonica
+##Ingreso de Datos
+- Tiempo_Llamada
+- Costo_Minuto
+- Costo_Llamada
+
+##Proceso
+- Pedir El tiempo de la Llamada
+- Pedir el Costo por Minuto
+- Multiplicar  Tiempo_Llamada por Costo_Minuto y alamacenar en Costo_Llamada
+- Mostrar Costo_Llamada
+
+##fin
---
+++ main.js
@@ -0,0 +1,9 @@
+var tiempo_Llamada=null;
+var costo_Minuto=null;
+var costo_Llamada=null;
+
+tiempo_Llamada=prompt("Por favor ingresa el tiempo d ela llamada");
+costo_Minuto=prompt("Por favor ingresa el costo por minuto");
+
+costo_Llamada=(parseFloat(tiempo_Llamada)*parseFloat(costo_Minuto));
+  alert(costo_Llamada);
